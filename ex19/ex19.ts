8a15e9d070aef1e1cb629ef134cd0ae6fa5047d5
ndinhtien <tiennd234111e@st.uel.edu.vn>
2026-01-29 21:04:46 +0700

    Add files via upload

---
+++ ex19/ex19.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-ex19',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './ex19.html',
+  styleUrls: ['./ex19.css'],
+})
+export class Ex19 {
+
+}
---
+++ ex19/list-product/list-product.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-product',
+  standalone: true,
+  templateUrl: './list-product.html',
+  styleUrls: ['./list-product.css'],
+})
+export class ListProduct {
+
+}
